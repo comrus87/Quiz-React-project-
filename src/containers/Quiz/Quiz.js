4582ef4ffe04dd0de070a2e4bb3e83b0a2f00291
Andrew <nk-andrew1987@mail.ru>
2019-09-26 17:40:55 +0300

    Подсветка

--- src/containers/Quiz/Quiz.js
+++ src/containers/Quiz/Quiz.js
@@ -5,6 +5,7 @@ import ActiveQuiz from '../../components/ActiveQuiz/ActiveQuiz'
 class Quiz extends Component {
 	state = {
 		activeQuestion: 0,
+		answerState: null,
 		quiz: [
 		  {
 		  	question: 'Какого цвета небо?',
@@ -32,10 +33,36 @@ class Quiz extends Component {
 	}
 
 	onAnswerClickHandler = (answerId) => {
-		console.log('Answer Id: ', answerId)
-		this.setState({
-			activeQuestion: this.state.activeQuestion + 1
-		})
+
+		const question = this.state.quiz[this.state.activeQuestion];
+
+		if (question.rightAnswerId === answerId) {
+
+			this.setState({
+				answerState: {[answerId]: 'success'}
+			})
+
+			const timeout = window.setTimeout(()=> {
+				if (this.isQuizFinished()) {
+					console.log('Finished')
+				} else {
+					this.setState({
+						activeQuestion: this.state.activeQuestion + 1
+					})
+				}
+
+				window.clearTimeout(timeout)
+			}, 1000);
+
+		} else {
+			this.setState({
+				answerState: {[answerId]: 'error'}
+			})
+		}
+	}
+
+	isQuizFinished () {
+		return this.state.activeQuestion + 1 === this.state.quiz.length
 	}
 
 	render () {
@@ -50,6 +77,7 @@ class Quiz extends Component {
 		 		  onAnswerClick={this.onAnswerClickHandler}
 		 		  quizLength={this.state.quiz.length}
 		 		  answerNumber={this.state.activeQuestion + 1}
+		 		  state={this.state.answerState}
 		 		 />
 		 	</div>
 		 </div>
